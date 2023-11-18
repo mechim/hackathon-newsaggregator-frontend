bb2023266a01324807720500c369b1db810b2a92
micha <michael.echim@gmail.com>
2023-11-18 23:05:35 +0200

    started working on news

--- news-aggregator/src/pages/News.jsx
+++ news-aggregator/src/pages/News.jsx
@@ -1,6 +1,26 @@
-import React, { useState} from 'react';
+import React, { useEffect, useState} from 'react';
 import Navbar from '../components/Navbar'
 function News() {
+    useEffect(() => {
+        fetchNews();
+    }, [])
+
+    const fetchNews = async() => {
+        try{
+            const token = await localStorage.getItem('access-token');
+            const headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json', // Adjust content type as needed
+                'Authorization': 'Bearer ' + token
+            };
+            axios.get('http://127.0.0.1:8000/posts/get-filtered', {headers}).then((res) => {
+                console.log(res.data);
+            })
+        } catch (e) {
+            console.error('error', e);
+        }
+        
+    }
     return(
         <>
         <Navbar/>
