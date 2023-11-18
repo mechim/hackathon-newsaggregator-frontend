c1ae3f999727e761e2982a669d9d38b50c2b8ce3
maria-afteni <marianaafteni2003@gmail.com>
2023-11-18 19:52:52 +0200

    Styled Register page

--- news-aggregator/src/pages/SignUp.jsx
+++ news-aggregator/src/pages/SignUp.jsx
@@ -1,17 +1,17 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { Form, Input, Button } from 'antd';
 import axios from 'axios';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './SignUp.css'
 function SignUp() {
     const [hasResponse, setHasResponse] = useState(false);
     const navigate = useNavigate();
-    const onFinish = async(values) => {
+    const onFinish = async (values) => {
         const request = await axios.post('http://127.0.0.1:8000/authen/sign-up', values).then((res) => {
             console.log(res.data);
             setHasResponse(true);
             navigate('/cabinet');
-        }).catch((e) =>{
+        }).catch((e) => {
             console.error(e);
         });
         console.log('Success:', values);
@@ -23,100 +23,101 @@ function SignUp() {
 
     const [requiredMark, setRequiredMarkType] = useState('optional');
     const onRequiredTypeChange = ({ requiredMarkValue }) => {
-      setRequiredMarkType(requiredMarkValue);
+        setRequiredMarkType(requiredMarkValue);
     };
 
     return (
-        <div className='background_box'>
-            <Form 
-                name="basic"
-                labelCol={{
-                    span: 8,
-                }}
-                wrapperCol={{
-                    span: 16,
-                }}
-                style={{
-                    marginTop: "30%",
-                    marginLeft: "25%",
-                    maxWidth: "60%",
-                    alignItems: "center",
-                }}
-                initialValues={{
-                    remember: true,
-                    requiredMarkValue: requiredMark,
-                }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-                colon = {false}
-                requiredMark={requiredMark === 'customize' ? customizeRequiredMark : requiredMark}
-            >
-                <Form.Item
-                    className = 'input_name'
-                    label={<span className='input_name'>Username</span>}
-                    name="username"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your username!',
-                        },
-                    ]}
-                    
-                    // style = {{
-                    //     fontFamily: 'Istok Web',
-                    //     fontStyle: normal,
-                    //     fontWeight: 400,
-                    //     fontSize: "32px",
-                    //     lineHeight: "46px",
-                    // }}
-                >
-                    <Input />
-                </Form.Item>
+        <div>
+            <div className='box1'>
 
-                <Form.Item
-                    
-                    label="Email"
-                    name="email"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your email',
-                            type: 'email'
-                        }
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
+            </div>
 
-                <Form.Item
-                   
-                    label="Password"
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your password!',
-                        },
-                    ]}
-                >
-                    <Input.Password />
-                </Form.Item>
-
-
-                <Form.Item
+            <div className='background_box'>
+                <Form
+                    className='form'
+                    name="basic"
+                    labelCol={{
+                        span: 8,
+                    }}
                     wrapperCol={{
-                        offset: 8,
                         span: 16,
                     }}
+                    initialValues={{
+                        remember: true,
+                        requiredMarkValue: requiredMark,
+                    }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                    colon={false}
+                    requiredMark={requiredMark === 'customize' ? customizeRequiredMark : requiredMark}
                 >
-                    <Button type="primary" htmlType="submit">
-                        Submit
-                    </Button>
-                </Form.Item>
-            </Form>
+                    <h1 className='header' style={{ marginLeft: "32%"}}>Registrare</h1>
+                 
+                    <div className='input_wraper'>
+                        <Form.Item
+                            className='input_name'
+                            label={<span className='input_name'>Login</span>}
+                            name="username"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your username!',
+                                },
+                            ]}
 
 
+
+                        >
+                            <Input style={{ alignItems: "center" }} />
+                        </Form.Item>
+
+                        <Form.Item
+
+                            label={<span className='input_name'>Email</span>}
+                            name="email"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your email',
+                                    type: 'email'
+                                }
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+
+                            label={<span className='input_name'>Parola</span>}
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your password!',
+                                },
+                            ]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+
+
+                        <Form.Item
+                            
+                            wrapperCol={{
+                                offset: 8,
+                                span: 16,
+                            }}
+                        >
+                            <Button type="primary" htmlType="submit" className="register_button">
+                                Submit
+                            </Button>
+                        </Form.Item>
+                    </div>
+                </Form>
+
+
+            </div>
         </div>
     )
 }
