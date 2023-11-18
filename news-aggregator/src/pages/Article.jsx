33e6c2ae5a9b318e9172aad97840fab9fb779e48
micha <michael.echim@gmail.com>
2023-11-19 00:21:22 +0200

    single article

--- news-aggregator/src/pages/Article.jsx
+++ news-aggregator/src/pages/Article.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Navbar from '../components/Navbar';
 function Article() {
     const [post, setPost] = useState({});
     useEffect(() => {
@@ -25,8 +26,10 @@ function Article() {
     }
     return(
         <>
+        <Navbar/>
         <h1>{post.title}</h1>
         <p>{post.body}</p>
+        <a href={post.link}>{post.link}</a>
         <p>{post.timestamp}</p>
         </>
     )
