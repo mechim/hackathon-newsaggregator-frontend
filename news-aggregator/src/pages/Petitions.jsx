6ec506e8b190abfcaed4bdc229a352b14f787dfd
micha <michael.echim@gmail.com>
2023-11-19 04:36:22 +0200

    creaza petitia

--- news-aggregator/src/pages/Petitions.jsx
+++ news-aggregator/src/pages/Petitions.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, Spin, Card } from 'antd';
+import { Button, Spin, Card, Form , Input} from 'antd';
 import Navbar from "../components/Navbar";
 import axios from 'axios';
 
@@ -14,7 +14,7 @@ function Petitions() {
         fetchPetitions();
     }, [])
     const handleCreate = async () => {
-        
+        setCreating(!creating);
     }
     const handleSubscription = async (id) => {
         try {
@@ -32,6 +32,26 @@ function Petitions() {
             console.error('error in subscribing', error);
         }
     }
+    const onFinish = async(values) => {
+        try{
+            const token = await localStorage.getItem('access-token');
+            const headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            };
+            await axios.post('http://127.0.0.1:8000/petitions/create/', values, {headers}).then((res) => {
+                console.log(res.data);
+                handleCreate();
+            })
+        } catch (e) {
+            console.log(values);
+            console.error('error in creating petition');
+        }
+    }
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
 
     const fetchPetitions = async () => {
         try {
@@ -62,7 +82,54 @@ function Petitions() {
             ) : (
                 <>
                     <h1>Petitii active</h1>
-                    <Button type="primary" style={{ marginBottom: '16px' }}>Creaza Petitie</Button>
+                    <Button type="primary" style={{ marginBottom: '16px' }} onClick={() => handleCreate()}>Creaza Petitie</Button>
+                    {creating ? <div>
+                        <Form
+                            className='form'
+                            name="basic"
+                            labelCol={{
+                                span: 8,
+                            }}
+                            wrapperCol={{
+                                span: 16,
+                            }}
+                            initialValues={{
+                                remember: true,
+                            }}
+                            onFinish={onFinish}
+                            onFinishFailed={onFinishFailed}
+                            autoComplete="off"
+                            colon={false}
+                        >
+                            <Form.Item
+                                label="Titlul"
+                                name="title"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Introduceti va rog titlul petitiei',
+                                    }
+                                ]}>
+                                <Input/>
+                            </Form.Item>
+                            <Form.Item
+
+                            label="Text"
+                            name="body"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Introduceti va rog descriptia petitiei',
+                                }
+                            ]}
+                            >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" >Creaza Petitia</Button>
+                        </Form.Item>
+                        </Form>
+                    </div> : <></>}
                     <div style={{ display: 'flex', flexWrap: 'wrap' }}>
                         {petitions.map(petition => (
                             <Card
